7c13f595a84bee9b9ee89cdee288c01a0a2a5067
PedroPassalacqua <pedropassalacqua@hotmail.com>
2023-06-21 15:37:26 -0300

    Delete CalculoCDB.API directory

--- CalculoCDB.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CalculoCDB.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- CalculoCDB.API/Models/Calculo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CalculoCDB
-{
-    public class Calculo
-    {
-        public int Mes { get; set; }
-        public double Valor { get; set; }
-    }
-}
